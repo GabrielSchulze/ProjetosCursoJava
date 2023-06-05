9e4eaf03a52de04a514d03630a0a3f95dc6fcab6
GabrielSchulze <gabrielschulze@hotmail.com>
2023-06-05 19:33:06 -0300

    Novo arquivo 'Temperatura'

--- src/fundamentos/Temperatura.java
+++ src/fundamentos/Temperatura.java
@@ -5,6 +5,6 @@ public class Temperatura {
 		//Fórmula para conversão de Fº em Cº: (Fº - 32) x 5/9 = Cº
 		double F = 37;
 		double C = (F - 32) * 5.0/9.0;
-		System.out.println(C + " Cº");
+		System.out.println(C + "ºC");
 	}
 }
