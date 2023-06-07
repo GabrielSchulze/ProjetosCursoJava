849cc0d300ec9342541f57d825ad45a083d0f6b8
GabrielSchulze <gabrielschulze@hotmail.com>
2023-06-06 21:23:25 -0300

    Atualizando arquivo 'Temperatura'

---
+++ src/fundamentos/NotacaoPonto.java
@@ -0,0 +1,20 @@
+package fundamentos;
+
+public class NotacaoPonto {
+
+	public static void main(String[] args) {
+		String s = "Bom dia X";
+		
+		//Seta todas as letras em maiúsculo
+		s = s.toUpperCase();		
+		System.out.println(s);
+		
+		//Substitui todo 'X' por 'Senhora'
+		s = s.replace("X", "Senhora");
+		System.out.println(s);
+		
+		//Concatena
+		s = s.concat("!!!");
+		System.out.println(s);
+	}
+}
