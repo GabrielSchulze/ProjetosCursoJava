7f24720ea2e8c6c414796a52c2bd02e903e2b61c
GabrielSchulze <gabrielschulze@hotmail.com>
2023-06-07 22:09:42 -0300

    Novo arquivo 'Import'

--- src/fundamentos/Import.java
+++ src/fundamentos/Import.java
@@ -8,6 +8,7 @@ public class Import {
 
 	public static void main(String[] args) {
 		Date d = new Date();
-		JButton botao = new JButton(); 
+		JButton botao = new JButton();
+		System.out.println(d);
 	}
 }
